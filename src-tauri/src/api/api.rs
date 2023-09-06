79879624ff54ddead9ab2217bc65c8d0b8b837df
zero <166997982@qq.com>
2023-09-06 20:57:35 +0800

    edit static var

--- src-tauri/src/api/api.rs
+++ src-tauri/src/api/api.rs
@@ -1,14 +1,12 @@
 use std::borrow::ToOwned;
 use std::fs;
-use std::fs::{copy, File};
+use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 
 use tauri::api::path;
-use tauri::api::path::home_dir;
 
 use crate::model;
-
 
 #[cfg(target_os = "windows")]
 const CONF_PATH_DB: &str = "\\.config\\g_email\\g_email.db";
@@ -20,19 +18,27 @@ const CONF_PATH_DB: &str = "/.config/g_email/g_email.db";
 #[cfg(target_os = "linux")]
 const CONF_PATH_FILE: &str = "/.config/g_email/g_email.yaml";
 
-static mut CONF_PATH: &str = home_dir().expect("获取用户目录错误").to_str().unwrap();
+static mut CONF_PATH: &str = "";
 
 //  初始化配置文件
 #[tauri::command]
 pub fn init_info() -> Result<(), String> {
-    let home_db_path = format!("{}{}", path::home_dir()
-        .expect("获取用户目录错误")
-        .to_str()
-        .expect("PathBuf to str err"), CONF_PATH_DB);
-    let home_file_path = format!("{}{}", path::home_dir()
-        .expect("获取用户目录错误")
-        .to_str()
-        .expect("PathBuf to str err"), CONF_PATH_FILE);
+    let home_db_path = format!(
+        "{}{}",
+        path::home_dir()
+            .expect("获取用户目录错误")
+            .to_str()
+            .expect("PathBuf to str err"),
+        CONF_PATH_DB
+    );
+    let home_file_path = format!(
+        "{}{}",
+        path::home_dir()
+            .expect("获取用户目录错误")
+            .to_str()
+            .expect("PathBuf to str err"),
+        CONF_PATH_FILE
+    );
     let cfg_db_path = Path::new(home_db_path.as_str());
     let cfg_file_path = Path::new(home_file_path.as_str());
     if let Some(parent) = cfg_file_path.parent() {
@@ -63,11 +69,17 @@ fn init_yaml() -> Result<(), String> {
     let yaml = serde_yaml::to_string(&data);
     return match yaml {
         Ok(res) => {
-            let file_path = format!("{}{}", path::home_dir()
-                .expect("获取用户目录错误")
-                .to_str()
-                .expect("PathBuf to str err"), CONF_PATH_FILE);
-            let mut file = File::options().write(true)
+            let file_path = format!(
+                "{}{}",
+                path::home_dir()
+                    .expect("获取用户目录错误")
+                    .to_str()
+                    .expect("PathBuf to str err"),
+                CONF_PATH_FILE
+            );
+            unsafe { CONF_PATH = Box::leak(file_path.to_owned().into_boxed_str()) }
+            let mut file = File::options()
+                .write(true)
                 .read(true)
                 .create(true)
                 .open(file_path)
@@ -75,13 +87,12 @@ fn init_yaml() -> Result<(), String> {
             file.write_all(&mut res.as_bytes()).expect("写入信息错误");
             Ok(())
         }
-        Err(_) => {
-            Err("序列化字符串错误".into())
-        }
+        Err(_) => Err("序列化字符串错误".into()),
     };
 }
 
 #[tauri::command]
 pub fn save_email_info(smtp_server: String, username: String, password: String) {
+    unsafe { println!("{}", CONF_PATH) };
     println!("{}{}{}", smtp_server, username, password);
 }
